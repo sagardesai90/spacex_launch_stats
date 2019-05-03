bbbb872abb85f1e76f712b5b9b728ad9aa750351
Sagar <saagardesai@gmail.com>
2019-05-02 22:12:46 -0500

    client boilerplate

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const cors = require('cors');
 const schema = require('./schema');
+const path = require('path');
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+// Allow cross-origin
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
     schema,
-    graphql: true
-}));
+    graphiql: true
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
