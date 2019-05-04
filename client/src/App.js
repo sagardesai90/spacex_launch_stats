1a3ef79bff761b09f743e338f1668c6c9eac5278
Sagar <saagardesai@gmail.com>
2019-05-04 14:02:50 -0500

    got cards to show

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import spacexLogo from './spacexLogo.png';
 import ApolloClient from 'apollo-boost';
 import {ApolloProvider} from "react-apollo";
+import Launches from './components/Launches';
 
 const client = new ApolloClient({
     uri: 'http://localhost:5000/graphql'
@@ -11,10 +12,13 @@ class App extends Component {
 
   render () {
       return (
-          <div className="App">
+          <ApolloProvider client={client}>
+          <div className="container">
             <img src = {spacexLogo} alt = "SpaceX" style = {{width: 300, display: 'block', margin: 'auto'}}
             />
+            <Launches />
           </div>
+          </ApolloProvider>
     );
   }
 
